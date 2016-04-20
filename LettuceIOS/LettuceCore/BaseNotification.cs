0e217a7857377245d4821fb8616e983127aac5db
Dave Vronay <davevr@eweware.com>
2016-04-20 09:37:25 -0700

    added notification DB connector

--- LettuceIOS/LettuceCore/BaseNotification.cs
+++ LettuceIOS/LettuceCore/BaseNotification.cs
@@ -4,9 +4,11 @@ namespace Lettuce.Core
 {
 	public class BaseNotification
 	{
+		public long id;
 		public DateTime	date;
 		public string	detail;
 		public int		type;
+		public bool		read;
 
 		public BaseNotification ()
 		{
